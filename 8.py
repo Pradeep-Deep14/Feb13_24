63e095befa3eb74d121c0fd7a835047178d47a55
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-02-13 18:01:12 +0530

    first commit

---
+++ 8.py
@@ -0,0 +1,10 @@
+List1=[1,2,5,6,8,10,12]
+List2=[]
+
+for x in range(1,15):
+    if x not in List1:
+        List2.append(x)
+    else:
+        print(List2)
+
+   
---
+++ 9.py
@@ -0,0 +1,4 @@
+List1 = [1, 2, 5, 6, 8, 10, 12]
+for i in range(1, 16):
+    if i not in List1:
+        print(i)
